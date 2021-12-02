0e4687a9fb7922ff99ddc8c678f54110bc2718df
Deepikageorgemohansundhar <60845929+Deepikageorgemohansundhar@users.noreply.github.com>
2021-12-02 15:39:25 +0530

    Create Archery.java

---
+++ Day3/Archery.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Archery {
+     static int turns,count;
+public int game(){
+Scanner sc=new Scanner(System.in);
+int target=100;
+
+for(int i=0;i<target;i++){
+System.out.println("Enter the turns:");
+turns+=sc.nextInt();
+count++;
+if(turns>=100){
+System.out.println("Target Reached");
+break;
+}
+}
+System.out.println("Total number of turns taken :"+count);
+return turns;
+}
+ 
+ 
+public static void main(String[] args) {
+
+Archery a=new Archery();
+a.game();
+}
+
+}
