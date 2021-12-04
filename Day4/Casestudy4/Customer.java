42de77799d9a930fd6c16c3b23838ce5e93c6a61
Deepikageorgemohansundhar <60845929+Deepikageorgemohansundhar@users.noreply.github.com>
2021-12-04 11:43:02 +0530

    Update Customer.java

--- Day4/Casestudy4/Customer.java
+++ Day4/Casestudy4/Customer.java
@@ -1,1 +1,22 @@
 
+
+package Day4casestudy4;
+
+public class Customer {
+static String name;
+Address address;
+
+public Customer(){
+
+}
+public Customer(String name,Address address){
+this.name=name;
+this.address=address;
+}
+@Override
+public String toString(){
+
+return name+" "+" "+address.toString();
+}
+
+}
