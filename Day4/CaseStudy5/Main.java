aa9c31fd9e3ee372d771b9f00e9176065de6f9ee
Deepikageorgemohansundhar <60845929+Deepikageorgemohansundhar@users.noreply.github.com>
2021-12-04 13:11:19 +0530

    Update Main.java

--- Day4/CaseStudy5/Main.java
+++ Day4/CaseStudy5/Main.java
@@ -1,1 +1,27 @@
 
+
+package Day4CaseStudy5;
+
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+  Address address=new Address();
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter Address Line 1");
+  address.setAddressLine1(sc.nextLine());
+  System.out.println("Enter Address Line 2");
+  address.setAddressLine2(sc.nextLine());
+  System.out.println("Enter the City Name");
+  address.setCity(sc.next());
+  System.out.println("Enter the State Name");
+  address.setState(sc.next());
+  System.out.println("Enter the Pincode");
+  address.setPincode(sc.nextInt());
+  System.out.println("The Address Details are");
+  System.out.println(address.toString());
+
+}
+}
